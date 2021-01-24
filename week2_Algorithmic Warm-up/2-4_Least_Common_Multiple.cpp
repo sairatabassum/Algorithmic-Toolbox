dbba249b031bc409a972528a4f853b2852938110
Saira Tabassum <52861859+sairatabassum@users.noreply.github.com>
2021-01-24 22:44:30 +0600

    Update 2-4_Least_Common_Multiple.cpp

--- week2_Algorithmic Warm-up/2-4_Least_Common_Multiple.cpp
+++ week2_Algorithmic Warm-up/2-4_Least_Common_Multiple.cpp
@@ -5,7 +5,7 @@ long long int gcd(long long int a, long long int b)
     if(b==0)
         return a;
     ///If a = b*q + r and b≠0 then GCD(a,b) = GCD(b,r)
-    /// where q is an integer, r is a reminder
+    /// where q is an integer, r is a remainder
     return gcd(b,a%b);
 
 }
