b7e1bdec9ab760f24d3dc5fdb67884254c48ab6f
Saira Tabassum <52861859+sairatabassum@users.noreply.github.com>
2021-01-24 22:05:23 +0600

    Add files via upload

---
+++ week2_Algorithmic Warm-up/2-4_Least_Common_Multiple.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long int gcd(long long int a, long long int b)
+{
+    if(b==0)
+        return a;
+    ///If a = b*q + r and b≠0 then GCD(a,b) = GCD(b,r)
+    /// where q is an integer, r is a reminder
+    return gcd(b,a%b);
+
+}
+int main()
+{
+    long long int a,b;
+    cin>>a>>b;
+    ///lcm= (a*b)/gcd(a,b)
+    cout<<(a*b)/(gcd(a,b))<<endl;
+}
+
